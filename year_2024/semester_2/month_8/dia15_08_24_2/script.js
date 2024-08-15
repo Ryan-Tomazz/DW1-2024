12d3dbe73b6a5aba43b8d75bffe007ca601ecd26
alduin10 <163424879+alduin10@users.noreply.github.com>
2024-08-15 11:46:23 -0300

    15

---
+++ year_2024/semester_2/month_8/dia15_08_24_2/script.js
@@ -0,0 +1,14 @@
+document.write("olla mundo")
+
+console.log("ollla mundo")
+
+alert("erro errro")
+
+// variaveis
+let x1
+var x2
+const x3 = 10
+
+// entrada
+let nome = prompt("digite seu nome")
+document.write("Seja bem vindo," + nome)
