ffff5b863fba3a13304e27e112f9cffd561075a5
alduin10 <ryantomaztalavieira@gmail.com>
2024-08-22 11:52:31 -0300

    22_08_24

--- year_2024/semester_2/month_8/dia22_08_24/script.js
+++ year_2024/semester_2/month_8/dia22_08_24/script.js
@@ -1,5 +1,16 @@
-let peso = Number(prompt("Digite seu peso"))
-let altura = Number(prompt("Digite sua altura"))
-let imc = peso / (altura ** 2)
+let inputweight = document.getElementById("weight")
+let inputheigt = document.getElementById("height")
+let divexit = document.getElementById("exit")
+let inputcalc = document.getElementById("calc")
 
-alert("IMC ="+ imc)
+inputcalc.onclick = calcularImc
+
+
+function calcularImc(){
+    let weight = Number(inputweight.value)
+    let height = Number(inputheigt.value)
+    let imc = weight / (height ** 2)
+    
+    //document.write("IMC ="+ imc)//
+    divexit.innerText = imc
+}
