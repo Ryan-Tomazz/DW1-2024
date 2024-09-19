fcb378b0ba60946248bf89b1cad63cff73fdab3e
alduin10 <ryantomaztalavieira@gmail.com>
2024-09-19 11:49:30 -0300

    19_09_2024

---
+++ year_2024/semester_2/month_9/day19_09_2024/script.js
@@ -0,0 +1,16 @@
+function mostrarTabuada(n) {
+    let i = 0
+    while (i <= 10) {
+        console.log(`${n} * ${i} = ${i * n}`)
+        i++
+    }
+    console.log()
+}
+
+function mostrarTabuada(n) {
+    // inicialização - condição - incremento
+    for (let i = 0; i <= 10; i++) {
+        console.log(`${n} * ${i} = ${i * n}`)
+    }
+    console.log()
+}   
