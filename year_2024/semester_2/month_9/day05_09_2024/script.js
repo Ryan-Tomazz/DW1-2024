f791026cd5a7ec3dd1620eea2fa471f982792058
alduin10 <ryantomaztalavieira@gmail.com>
2024-10-03 10:08:11 -0300

    03_10_24

--- year_2024/semester_2/month_9/day05_09_2024/script.js
+++ year_2024/semester_2/month_9/day05_09_2024/script.js
@@ -12,15 +12,19 @@ function calcularMulta() {
     let multa = Number(velocidadeCarro / velocidadeMax)
     let multaPorcentagem = multa * 100
     let porcentagemMulta = multaPorcentagem - 100
+    let porcentagemMultaInteiro = Math.trunc(porcentagemMulta)
 
     if (porcentagemMulta > 50) {
-        divexit.innerHTML= "Você excedeu + de " + porcentagemMulta + "% da velocidade máxima. Sua multa é de RS 880,41"
+        divexit.innerHTML= "Você excedeu + de " + porcentagemMultaInteiro + "% da velocidade máxima. Sua multa é de RS 880,41"
     }
     if (porcentagemMulta > 20 && porcentagemMulta < 50) {
-        divexit.innerText =  "Você excedeu + de " + porcentagemMulta + "% da velocidade máxima. Sua multa é de RS 195,23"
+        divexit.innerText =  "Você excedeu + de " + porcentagemMultaInteiro + "% da velocidade máxima. Sua multa é de RS 195,23"
     }
     if (porcentagemMulta <= 20){
-        divexit.innerText = "Você excedeu até " + porcentagemMulta + "% da velocidade máxima. Sua multa é de RS 130,16"
+        divexit.innerText = "Você excedeu até " + porcentagemMultaInteiro + "% da velocidade máxima. Sua multa é de RS 130,16"
+    }
+    if(porcentagemMulta <= 0){
+        divexit.innerText = "Você não excedeu a velocidade"
     }
     
 }
