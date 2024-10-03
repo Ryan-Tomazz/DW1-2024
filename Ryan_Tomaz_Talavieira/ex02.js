e74377fbc27a1a726caac4d9d70be9bd64488a54
alduin10 <ryantomaztalavieira@gmail.com>
2024-10-03 17:46:19 -0300

    03_10_24

--- Ryan_Tomaz_Talavieira/ex02.js
+++ Ryan_Tomaz_Talavieira/ex02.js
@@ -1,22 +1,20 @@
 //]Escreva uma função que receba 3 números inteiros e devolva o menor entre eles.
-let x = 0
-let y = 0
-let z = 0
+
 function menor(x,y,z){
    
   if(x == y == z){
       console.log( "os números são iguais, logo não há um menor")}
-  if(x = y){
+  if(x == y){
     if(z < x){
         console.log(z+" é o menor")
     }
   }
-  if(x = z){
+  if(x == z){
     if(y <z){
         console.log(y+" é o menor")
     }
   } 
-  if(z = y){
+  if(z == y){
     if(x<y){
         console.log(x + " é o menor")
     } 
@@ -38,4 +36,4 @@ function menor(x,y,z){
   }
 
 }
-console.log(menor(7,3,5))
+console.log(menor(10,10,20))
