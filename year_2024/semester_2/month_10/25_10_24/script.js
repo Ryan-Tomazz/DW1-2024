f17540b850f120325965d44856c2bae344647a3c
alduin10 <ryantomaztalavieira@gmail.com>
2024-10-25 15:43:02 -0300

    25_10_24

---
+++ year_2024/semester_2/month_10/25_10_24/script.js
@@ -0,0 +1,10 @@
+console.log(reverseString("javascript")); // Deve exibir: "tpircsavaj"
+console.log(reverseString("hello")); // Deve exibir: "olleh"
+
+function reverseString(x){
+    let contX = x.length
+    let reverse = x
+    while(s <= contX){
+        
+    }
+}
