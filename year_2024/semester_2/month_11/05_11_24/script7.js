763187cba9b429b93dc3963a0129de816c42f74e
alduin10 <ryantomaztalavieira@gmail.com>
2024-11-05 10:16:38 -0300

    05_11_24

--- year_2024/semester_2/month_11/05_11_24/script7.js
+++ year_2024/semester_2/month_11/05_11_24/script7.js
@@ -1,8 +1,16 @@
 function mescli(x,y){
     s = x.length + y.length
-    for(i=0, v=""; i<s; i++){
-        v += x.
-    }
+    for(i=0, v="", j =""; i<s; i++){
+        if (i < x.length){
+            v= v + x[i] + " "}
+        if (i >= x.length && i == (s-1)){
+        v = v + y[(i - x.length)]} else{
+                if (i >= x.length){
+                v = v + y[(i - x.length)] + " "}
+            }
+    } 
+    v = v.split(" ")
+    return v 
 }
 
 console.log(mescli([1, 2], [3, 4])); // Deve exibir: [1, 2, 3, 4]
